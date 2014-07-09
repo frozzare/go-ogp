f1c97936b92d216a3ca48083da28bd497865a5c4
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2014-07-09 17:29:22 +0200

    First commit

---
+++ og.go
@@ -0,0 +1,51 @@
+package og
+
+import (
+  "net/http"
+  "io/ioutil"
+  "log"
+  "regexp"
+  "fmt"
+)
+
+func Fetch (url string) map[string]interface{} {
+  res, err := http.Get(url)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  defer res.Body.Close()
+  body, err := ioutil.ReadAll(res.Body)
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  r, _ := regexp.Compile("<meta.*property=\"og:(.*)\".*content=\"(.*)\".*>")
+  matches := r.FindAllStringSubmatch(string(body), -1)
+
+  list := make(map[string]interface{})
+
+  for i := range matches {
+    key := matches[i][1]
+    value := matches[i][2]
+    list[key] = value
+  }
+
+  return list
+}
+
+func Print (url string) {
+  list := Fetch(url)
+
+  if (list["title"] != nil) {
+    fmt.Printf("\n\x1b[33;1m%s\x1b[0m\n", list["title"])
+  }
+
+  for key, value := range list {
+    fmt.Printf("\n\x1b[32;1mog:%s\x1b[0m\n%s\n", key, value)
+  }
+
+  fmt.Printf("\n")
+}
---
+++ og_test.go
@@ -0,0 +1,13 @@
+package og
+
+import (
+  "testing"
+  "github.com/bmizerany/assert"
+)
+
+func TestFetch (t *testing.T) {
+  list := Fetch("http://ogp.me")
+  assert.Equal(t, "Open Graph protocol", list["title"])
+  assert.Equal(t, "website", list["type"])
+  assert.Equal(t, "http://ogp.me/", list["url"])
+}
